55fc6baacb29c59f989297a94339090046cc9f7a
p32424 <faeryqueen21@fullsail.edu>
2013-05-29 18:19:22 -0700

    Added stub code.

--- Antonio_Ariana_assignment4/Antonio_Ariana_Assignment4.js
+++ Antonio_Ariana_assignment4/Antonio_Ariana_Assignment4.js
@@ -1,2 +1,29 @@
 // alert("JavaScript works!");
 
+/* Ariana Antono
+   SDI 1305
+   Project 4
+   My code library
+*/
+
+var myLibrary = function() {
+    
+    // Check phone number string function
+    
+    // Check URL string function
+    
+    // Format number of decimals function
+    
+    // Convert string number to actual number function
+    
+    // Smallest value great than given value in array function
+    
+    // Find total value of just numbers in an array function
+    
+    
+}; // end myLibrary
+
+//outputs to test code
+var newLib = new myLibrary();
+
+
