8bf771b2aa3d63804553261496c39bdc93590011
p32424 <faeryqueen21@fullsail.edu>
2013-07-14 22:53:34 -0700

    Added some CSS style.

--- VFW Projects/Antonio_Ariana_project2/js/main.js
+++ VFW Projects/Antonio_Ariana_project2/js/main.js
@@ -1,9 +1,5 @@
-// Hello World!
-// Author: Ariana Antonio
-// Created For: SDI Online
-// Introductory look into JavaScript using the Firebug console
-
-console.log("Hello World!");
-
-console.log("Test test");
-console.log(42);
+/*
+Author: Ariana Antonio
+Project 2: Movie Journal
+VFW 1307
+*/
