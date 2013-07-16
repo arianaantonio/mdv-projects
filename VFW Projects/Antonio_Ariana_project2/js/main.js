59b5ea562599701aca727469c72ea2e0cbb29b16
p32424 <faeryqueen21@fullsail.edu>
2013-07-15 23:04:13 -0700

    Added gwtElementById function, dynamic select, and link click events.

--- VFW Projects/Antonio_Ariana_project2/js/main.js
+++ VFW Projects/Antonio_Ariana_project2/js/main.js
@@ -3,3 +3,42 @@ Author: Ariana Antonio
 Project 2: Movie Journal
 VFW 1307
 */
+//DOM Loading
+window.addEventListener("DOMConteLoaded", function(){
+    
+    //Generic function to grab elements
+    function id(x){
+        var getElement = document.getElementById(x)
+        return getElement;
+    }
+    //Dynamic select function
+    var movieGenre = ["Genre", "Comedy", "Action", "Drama", "Romance", "Horror", "Other"]
+
+    //Link click events
+    var displayDataLink = id("displayData");
+    displayDataLink.addEventListener("click", pullData);
+    var clearDataLink = id("clearData");
+    clearDataLink.addEventListener("click", clearLocalData);
+    var saveMovie = id("logMovie");
+    saveMovie.addEventListener("click", saveData);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
