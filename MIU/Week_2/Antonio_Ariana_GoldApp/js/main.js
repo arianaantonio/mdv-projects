4af406c4c7bd6f8a096a92c52678f345b83f9279
p32424 <faeryqueen21@fullsail.edu>
2013-09-12 17:36:45 -0700

    Added dynamic search function to index page.

--- MIU/Week_2/Antonio_Ariana_GoldApp/js/main.js
+++ MIU/Week_2/Antonio_Ariana_GoldApp/js/main.js
@@ -270,6 +270,92 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     }
     
+    //Search button
+    var search = id("searchBtn");
+    search.addEventListener("click", searchApp);
+    
+    function searchApp () {
+        //var genres = id("genre").value;
+        var text = id("search").value;
+        /*
+        //Search by genre only
+        if (genres != "Genre" && text=="") {
+            var createDiv = document.createElement("div");
+            createDiv.setAttribute("id", "values");
+            var createList = document.createElement("ul");
+            createDiv.appendChild(createList);
+            document.body.appendChild(createDiv);
+            for (var i=0, j=localStorage.length; i<j; i++) {
+                var createLi = document.createElement("li");
+                var linksList = document.createElement("li");
+                createList.appendChild(createLi);
+                var key = localStorage.key(i);
+                var keyValue = localStorage.getItem(key);
+                var obj = JSON.parse(keyValue);
+                var newList = document.createElement("ul");
+                createLi.appendChild(newList);
+                if(genres === obj.genre[1]) {
+                    for (l in obj){
+                        var newLi = document.createElement("li");
+                        newList.appendChild(newLi);
+                        var optText = obj[l][0] + " " + obj[l][1];
+                        newLi.innerHTML = optText;
+                        newList.appendChild(linksList);
+                        console.log(obj[l][0] + ": "+ obj[l][1]);
+                    }
+                }
+            }
+        }*/
+        //Search by text only
+        if (text != "") {
+            var createDiv = document.createElement("div");
+            createDiv.setAttribute("id", "values");
+            var createList = document.createElement("ul");
+            createDiv.appendChild(createList);
+            var createSearch = id("searchForm");
+            createSearch.appendChild(createDiv);
+            for (var i=0, j=localStorage.length; i<j; i++) {
+                var createLi = document.createElement("li");
+                //var linksList = document.createElement("li");
+                createList.appendChild(createLi);
+                var key = localStorage.key(i);
+                var keyValue = localStorage.getItem(key);
+                var obj = JSON.parse(keyValue);
+                var newList = document.createElement("ul");
+                createLi.appendChild(newList);
+                for (l in obj) {
+                    if (text === obj[l][1]) {
+                        for(k in obj){
+                            var newLi = document.createElement("li");
+                            newList.appendChild(newLi);
+                            newLi.setAttribute("class", "newLiStyle")
+                            var optText = obj[k][0] + " " + obj[k][1];
+                            newLi.innerHTML = optText;
+                            //newList.appendChild(linksList);
+                            console.log(obj[k][0] + ": "+ obj[k][1]);
+                        }
+                    }
+                }
+            } 
+        }
+        /*
+        //Search by text and genre
+        if (text !="" && genres != "Genre") {
+            for (var i=0, j=localStorage.length; i<j; i++) {
+                var key = localStorage.key(i);
+                var keyValue = localStorage.getItem(key);
+                var obj = JSON.parse(keyValue);
+                for (l in obj) {
+                    if (genres === obj.genre[1] && text === obj[l][1]) {
+                        for(k in obj){
+                            console.log(obj[k][0] + ": "+ obj[k][1]);
+                        }    
+                    }
+                }
+            }
+        }*/
+    }
+    
     //variables
     var movieGenre = ["Genre", "Comedy", "Action", "Drama", "Romance", "Horror", "Other"],
         theaterValue,
