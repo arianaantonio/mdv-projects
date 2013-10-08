34b40bb72e0dc927aaea2ab44998b7a3baeefbd0
p32424 <faeryqueen21@fullsail.edu>
2013-10-07 18:43:56 -0700

    Refactoring search to jQuery.

--- ASD/Project_2/js/main.js
+++ ASD/Project_2/js/main.js
@@ -3,25 +3,54 @@ Author: Ariana Antonio
 Project 2
 ASD 1310
 */
-$('#home').on('pageinit', function(){
-	//var search = $('#searchBtn');
-        //search.addEventListener("click", searchApp);
-        $('#searchBtn').click(function(){
-                searchApp();
-        });
+$('#search').on('pageinit', function(){
+    var text = $("search").val();
+    function searchApp(){
+       if (text != "") {
+            var createDiv = document.createElement("div");
+            createDiv.setAttribute("id", "values");
+            var createList = document.createElement("ul");
+            createDiv.appendChild(createList);
+            var createSearch = $("searchForm");
+            createSearch.appendChild(createDiv);
+            for (var i=0, j=localStorage.length; i<j; i++) {
+                var createLi = document.createElement("li");
+                //var linksList = document.createElement("li");
+                createList.appendChild(createLi);
+                var key = localStorage.key(i);
+                var keyValue = localStorage.getItem(key);
+                var obj = JSON.parse(keyValue);
+                var newList = document.createElement("ul");
+                createLi.appendChild(newList);
+                for (l in obj) {
+                    if (text === obj[l][1]) {
+                        for(k in obj){
+                            var newLi = document.createElement("li");
+                            newList.appendChild(newLi);
+                            newLi.setAttribute("class", "newLiStyle")
+                            var optText = obj[k][0] + " " + obj[k][1];
+                            newLi.innerHTML = optText;
+                            //newList.appendChild(linksList);
+                            //console.log(obj[k][0] + ": "+ obj[k][1]);
+                        }
+                    }
+                }
+            } 
+        }
+    }
+    $('#searchBtn').click(function(){
+        searchApp();
+    });
 });		
 $('#addItem').on('pageinit', function(){
-
-		var myForm = $('#addItemForm');
-		    myForm.validate ({
-			invalidHandler: function(form, validator) {
-			},
-                         //errorPlacement: function(error, element) {
-                         //error.appendTo( element.parent("input").next("input") );
-                       // },
-			submitHandler: function() {
-		var key = myForm.serializeArray();
+    var myForm = $('#addItemForm');
+    myForm.validate ({
+		invalidHandler: function(form, validator) {
+		},
+		submitHandler: function() {
+		    var key = myForm.serializeArray();
 			storeData(this.key);
 		}
-	});
+    });
+});
 	
